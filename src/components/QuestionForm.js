19f8704aebdaa243339f540d9e5c933ded431273
Chan Lee <clee09818@gmail.com>
2022-10-23 18:03:31 -0400

    commit tree b0b0342f026a5d43134aedef3cec0306f3e015a6

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,6 +19,24 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    const itemData = {
+      prompt: formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      correctIndex: formData.correctIndex,
+    }
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+
+    .then((r) => r.json())
+    .then((newItem) => console.log(newItem));
+
     console.log(formData);
   }
 
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionForm from './QuestionForm';
+import QuestionItem from './QuestionItem';
 
 function QuestionList() {
+
+  const [question, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then((r) => r.json())
+      .then((questions) => setQuestions(questions))
+  }, [])
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = question.filter((q) => q.id !== deletedItem.id);
+    setQuestions(updatedItems);
+  }
+
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{question.map((q, index) => {
+
+        return (
+          <QuestionItem key={q.id} question={q} onDeleteItem={handleDeleteItem} />
+        )
+      })
+        
+        }
+        
+      </ul>
     </section>
   );
 }
